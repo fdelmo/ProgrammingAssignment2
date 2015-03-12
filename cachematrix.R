e0fe9e3b1b830a14a39adb69c0023bf7509abf26
fdelmo <ferrandelmoral@gmail.com>
2015-03-12 19:48:01 +0100

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix takes a matrix type argument and return a special
+## matrix object. cacheSolve calculates the inverse of the makeCacheMatrix
+##object and if it has been calculated before it returns the value from the cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix create as special matrix object.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    elc <- NULL
+    set <- function(y) {
+        x <<- y
+        elc <<- NULL
+    }
+    get <- function() x
+    setreverse<- function(reverse) elc <<-reverse
+    getreverse <- function() elc
+    list(set = set, get = get,
+         setreverse = setreverse,
+         getreverse = getreverse)
 }
-
-
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'. If this matrix has been
+##calculated before, it will be returned form the cache without calculate
+##it again.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    elc <- x$getreverse()
+    if (!is.null(elc)) {
+        message("getting cached reververse matrix")
+        return(elc)
+    } else {
+        elc <- solve(x$get())
+        x$setreverse(elc)
+        return(elc)
+    }
 }
